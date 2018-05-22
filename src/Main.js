678adde09c435a32c9dfb42a098574fd17c8098d
Kalpan Jasani <kjasani@purdue.edu>
2018-05-22 14:39:13 -0400

    style App

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from "./Sidebar.js";
+import NoteList from "./NoteList.js";
+import NoteForm from "./NoteForm.js"
+
+const Main = () => {
+    return(
+        <div className="Main">
+            <Sidebar />
+            <NoteList />
+            <NoteForm />
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/NoteList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NoteList = () => 
+{
+    return(
+        <div className="NoteList">
+            NoteList
+        </div>
+    )
+    
+
+}
+
+export default NoteList
